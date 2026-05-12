934c767004036d291a6a2638be548070ccceca9b
nikhilrana07 <kumarnikil220@gmail.com>
2026-05-12 18:08:20 +0530

    Api for user register

---
+++ src/main/java/com/project/fitness/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.project.fitness.controller;
+
+import com.project.fitness.model.User;
+import com.project.fitness.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+
+public class AuthController {
+
+    private final UserService userService;
+
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user) {
+        return  userService.registerUser(user);
+    }
+
+
+
+
+}
